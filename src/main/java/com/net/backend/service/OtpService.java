b2647f6f66821d76e12f7fcf84481da5cb7dc59c
suyash <shevadesuyash@gmail.com>
2025-02-19 18:22:22 +0530

    updated otp service

--- src/main/java/com/net/backend/service/OtpService.java
+++ src/main/java/com/net/backend/service/OtpService.java
@@ -1,4 +1,63 @@
 package com.net.backend.service;
 
+import com.net.backend.model.OtpData;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
 public class OtpService {
+    private Map<String, OtpData> otpMap = new HashMap<>();
+    private static final long EXPIRATION_TIME_MS = 5 * 60 * 1000; // 5 minutes
+
+    public static int generateOtp() {
+        return (int) (Math.random() * 900000) + 100000;
+    }
+
+    public String generateOtpForEmail(String email) {
+        int otp = generateOtp();
+        long currentTime = System.currentTimeMillis();
+        otpMap.put(email, new OtpData(otp, currentTime));
+        return "Your OTP for email verification is: " + otp;
+    }
+
+    public boolean verifyOtp(String email, int otp) {
+        if (otpMap.containsKey(email)) {
+            OtpData otpData = otpMap.get(email);
+            if (otpData.getOtp() == otp && !isOtpExpired(otpData)) {
+                otpMap.remove(email); // OTP verified, remove it
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void removeOtp(String email) {
+        otpMap.remove(email);
+    }
+
+    public int getOtp(String email) {
+        return otpMap.getOrDefault(email, new OtpData(-1, 0)).getOtp();
+    }
+
+    public boolean isOtpExpired(String email, int otpValidityDuration) {
+        OtpData otpData = otpMap.get(email);
+        if (otpData != null) {
+            long expirationTime = otpData.getTimestamp() + otpValidityDuration * 60000;
+            return System.currentTimeMillis() > expirationTime;
+        }
+        return true;
+    }
+
+    private boolean isOtpExpired(OtpData otpData) {
+        return (System.currentTimeMillis() - otpData.getTimestamp()) > EXPIRATION_TIME_MS;
+    }
+
+    public String sendOtpToMail(String email , String message){
+        // Implement sending OTP via email
+        return "OTP sent successfully!";
+    }
+
+
 }
